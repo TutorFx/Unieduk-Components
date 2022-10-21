c1e3b9b148323e7cf25f233ed20040b4ad5f49fc
TutorFx <gabriel_serejo@live.com>
2022-10-21 07:13:32 -0300

    Cards

--- src/components/select.js
+++ src/components/select.js
@@ -1,7 +1,11 @@
 const fajSelect = {
   template: /*html*/ `
   <div tabindex="0" class="faj-select">
-    {{titulo}}
+    <div>{{titulo}}</div>
+    <svg width="14" height="9" viewBox="0 0 14 9" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <path d="M12.8332 1.5L6.99984 7.33333L1.1665 1.5" stroke="#6F727A" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+    </svg>
+    
     <div class="faj-select--container">
       <div class="content">
         <div class="search-box">
