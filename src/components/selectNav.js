5a77140d377b0fa554de20c419dfbf4e566ba173
TutorFx <gabriel_serejo@live.com>
2022-11-07 17:42:41 -0300

    Selectnav two way binding gambiarra

--- src/components/selectNav.js
+++ src/components/selectNav.js
@@ -1,19 +1,26 @@
 const fajSelectNav = {
-    template: /* html */`
+  template: /* html */ `
     <div class="faj-select-nav-container">
-        <label class="faj-select-nav" v-for="i in options" :key="i">
-            <input type="radio" :name="uid" />
+        <label class="faj-select-nav" v-for="(i, num) in options" :key="i">
+            <input type="radio" :value="i"
+            @input="$emit('selected', $event.target.value)" 
+            v-model="selected"
+            :name="uid" />
             <div class="modded">{{i}}</div>
         </label>
     </div>
     `,
-    data: () => ({
-        uid: Date.now() + "-" + Math.floor(Math.random() * 100)
-    }),
-    props:{
-        options:{
-            default: ['teste1', 'teste2'],
-            typeof: Array
-        }
+  data: () => ({
+    uid: Date.now() + "-" + Math.floor(Math.random() * 100),
+    selected: null
+  }),
+  props: {
+    options: {
+        default: [],
+        typeof: Array,
     }
-}
+  },
+  created(){
+    this.selected = this.options[0]
+  }
+};
